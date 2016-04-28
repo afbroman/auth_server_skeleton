549a7347f5cc17e7a4ecea8775822d1b6dba7944
Andrew Broman <broman@acm.org>
2016-04-27 21:41:30 -0500

    Change return from controller

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -20,7 +20,7 @@ exports.signup = function(req, res, next) {
     user.save(function(err) {
       if (err) { return next(err); }
 
-      res.json(user);
+      res.json({ success: true });
     });
 
   });
