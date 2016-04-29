30f86e4f1c5a6c6276776252199a2f142056d5f6
Andrew Broman <broman@acm.org>
2016-04-28 21:22:47 -0500

    Add bcrypt for encrypting passwords

--- models/user.js
+++ models/user.js
@@ -1,11 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // Define the model
 const userSchema = new Schema({
   // this is literally the JS String
   email: { type: String, unique: true, lowercase: true },
   password: String
+});
+
+// On save hook, encrypt password
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err); }
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) { return next(err); }
+
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 // Create the model class
