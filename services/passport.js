d1b695788ca11a405a318e25b0a2d4f08e61cdc8
Andrew Broman <broman@acm.org>
2016-05-01 18:21:05 -0500

    Finish authentication server

--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,24 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// Create local strategy
+const localOptions = { usernameField: 'email' };
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+  User.findOne({ email: email }, function(err, user) {
+    if (err) { return done(err, false); }
+    if (!user) { return done(null, false); }
+
+    // compare passwords
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) { return done(err, false); }
+      if (!isMatch) { return done(null, false); }
+
+      return done(null, user);
+    });
+  });
+});
 
 // Set up options for JWT Strategy
 const jwtOptions = {
@@ -29,3 +47,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 
 // Tell passport to use strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
